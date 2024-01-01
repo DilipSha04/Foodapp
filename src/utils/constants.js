fddac2e9daaf0372bca823748361d3bdf9cd0967
DilipSha04 <imdilip.004@gmail.com>
2024-01-01 11:30:07 +0530

    update key to allow cors to anywhere

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,9 +4,9 @@ export const IMG_URL =
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=";
 
 
-//export const SWIGGY_API = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.462521&lng=85.8829895&page_type=DESKTOP_WEB_LISTING"
+export const SWIGGY_API = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.462521&lng=85.8829895&page_type=DESKTOP_WEB_LISTING"
 
-export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+//export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 
 
